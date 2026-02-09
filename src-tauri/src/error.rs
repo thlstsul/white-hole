cdc80229124d90d4e63ff63a5342e8418f2f3666
zhouzm5 <zhouzm5@asiainfo.com>
2026-02-09 16:19:13 +0800

    fix:优化错误展示

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -4,34 +4,40 @@ use crate::impl_serialize;
 
 error_set! {
     ParseError := {
+        #[display("无法解析的URL: {0}")]
         Url(url::ParseError),
+        #[display("无法解析的公共后缀: {0}")]
         PublicSuffix(publicsuffix::Error),
     }
     FrameworkError := {
+        #[display("Tauri 框架错误: {0}")]
         Tauri(tauri::Error),
     }
     DatabaseError := {
+        #[display("数据库执行错误: {0}")]
         Execute(sqlx::Error),
+        #[display("数据库迁移错误: {0}")]
         Migrate(sqlx::migrate::MigrateError),
     }
     SetupError := {
-        DbConnect(sqlx::Error),
+        #[display("后台进程设置错误: {0}")]
         Task(delay_timer::error::TaskError),
-        Migarate(sqlx::migrate::MigrateError),
-    } || FrameworkError || ParseError
+    } || DatabaseError || FrameworkError || ParseError
     TabError := StateError || FrameworkError || ParseError
     StateError := {
+        #[display("无法获取主视图")]
         NoMainView
     } || FrameworkError || DatabaseError || IconError
-    WindowError := {
-        WindowState(tauri_plugin_window_state::Error),
-    } || FrameworkError || StateError
     IconError := {
+        #[display("无法获取图标数据：{0}")]
         GetDataUrl(get_data_url::Error),
+        #[display("无法保存图标：{0}")]
         SaveIcon(sqlx::Error),
+        #[display("图标获取中")]
         Fetching,
     }
     SyncPublicSuffixError := {
+        #[display("无法获取公共后缀: {0}")]
         FetchPublicSuffix(reqwest::Error)
     } || DatabaseError
     GetPublicSuffixError := ParseError || DatabaseError
