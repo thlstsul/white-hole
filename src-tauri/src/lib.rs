890f11be18802fa0f9f91835f12a252489c4aaa0
THELOSTSOUL <1095533751@qq.com>
2026-09-02 23:25:07 +0800

    清空失焦时残留的已按下按键

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -188,15 +188,14 @@ fn on_window_event(window: &Window, event: &WindowEvent) {
             } else if let WindowEvent::Focused(true) = event {
                 // Webview::set_focus 后，会触发 WindowEvent::Focused 事件；所以 focus_changed 做了防抖
                 let browser = window.browser();
-                if let Ok(true) = browser
-                    .focus_changed()
-                    .await
-                    .inspect_err(|e| error!("聚焦变更失败：{e}"))
-                {
-                    // 窗口重新聚焦时，清空残留已按下按键
-                    let hotkey = window.hotkey();
-                    hotkey.clear_pressed();
+                if let Err(e) = browser.focus_changed().await {
+                    error!("聚焦变更失败：{e}");
                 }
+            } else if let WindowEvent::Focused(false) = event {
+                // 窗口重新失焦时，清空残留已按下按键
+                // TODO 目前只发现跨域时也会触发，原因未知
+                let hotkey = window.hotkey();
+                hotkey.clear_pressed();
             } else if let WindowEvent::ThemeChanged(theme) = event {
                 let browser = window.browser();
                 browser.update_theme(theme).await;
